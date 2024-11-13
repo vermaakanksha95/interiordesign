5a4e4d2bd257f71a082d7679118215baa5c2d980
ruchikushwah <ruchi741@bgpgmail.com>
2024-11-13 09:20:26 +0530

    homepage

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<div class="bg-[#0E3F54] w-full h-52   items-center justify-between flex">
+    <div class="bg-white h-24 py-5 flex flex-1 px-[10%] items-center justify-between"> 
+        <img src="images/02_Logo_H_1_2_-_SIZED.webp" alt="" class="w-44 h-[100px]">
+        <a href="">Home</a>
+        <a href="">shop</a>
+        <a href="">projects</a>
+        <a href="">contact us</a>
+    </div>
+
+
+</div>
--- index.php
+++ index.php
@@ -1,13 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>interior-design</title>
+    <?php include "includes/headlink.php" ?>
 
 </head>
+
 <body>
-    <h1>hello</h1>
-    
+    <?php include "includes/header.php"; ?>
+    <div class="bg-center  ">
+        <img src="images/furniture-design-service-38950023332066.webp" alt="" class="w-full h-[600px]">
+
+    </div>
+    <div class="bg-[#0E3F54] w-full h-full flex py-10">
+        <div class="w-6/12  py-5 flex items-center flex-col ">
+            <span class="text-3xl   font-Raleway font-serif text-white font-normal  text-left  py-[10%] ">WE ARE A SMALL<br> BUSINESS ROOTED<br> IN FAMILY</span>
+            <img src="images/carpentryshopcoAsset_71.webp" alt="" class="py-5 w-94 h-[700px] self-start">
+        </div>
+        <div class="w-6/12 ">
+
+            <img src="images/Untitled_design_2.webp" alt="" class="w-full h-[700px] -mt-20">
+            <div class="-mt-20 gap-5 flex flex-col">
+                <h2 class="text-2xl font-Raleway font-serif text-white">
+                    HIGH QUALITY CRAFTSMANSHIP.
+                </h2>
+                <p class="text-white text-lg font-Raleway  font-light">At the heart of everything we create is our deep dedication to<br> family. As parents, we understand the importance of spaces that <br>bring loved ones together. Our furniture is designed to enable and <br>encourage connection—whether it’s a dining table where ideas <br>are shared, a coffee table that hosts family meetings, or a desk <br>where dreams take shape.
+
+                    We are thrilled to expand our offerings<br> by carrying a curated selection of artisan-made furniture<br> and home decor. Through partnerships with skilled artisans around the<br> globe, we bring you unique, high-quality, made-to-order products <br>that add warmth and character to your home.
+
+                    Let us help you<br> create spaces where your family can gather, grow, and connect.</p>
+                <a href="" class="px-10 items-center flex">
+                    <div class="border border-[#D1A773] text-sm px-10 py-3 text-[#d1a773] self-start font-normal">LEARN MORE ABOUTE US</div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class=" px-10 py-5">
+        <h2 class="text-3xl text-orange-600">UNIQUE, HANDCRAFTED FURNISHINGS</h2>
+        <ul class="gap-5  px-10 py-5 grid grid-cols-4">
+            <li>
+                <div>
+                    <a href="" title="dining">
+                        <div>
+                            <img src="images/dining.webp" alt="" class="w-[500px] h-[400px]">
+                        </div>
+                        <div>
+                            <h3>dining</h3>
+                        </div>
+
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <a href="" title="table">
+                        <div>
+                            <img src="images/IMG_2791.webp" alt="" class="w-[500px] h-[400px]">
+                        </div>
+                        <div>
+                            <h3>table</h3>
+                        </div>
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <a href="" title="desk">
+                        <div>
+                            <img src="images/desk-38146958524642.webp" alt="" class="w-[500px] h-[400px]">
+                        </div>
+                        <div>
+                            <h3>desk</h3>
+                        </div>
+
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <a href="" title="fallon-sofa">
+                        <div>
+                            <img src="images/fallon-sofa-40227294216418_556ac4b8-921b-4150-853d-b703fc69be67.webp" alt="" class="w-[500px] h-[400px]">
+                        </div>
+                        <div>
+                            <h3>fallon-sofa</h3>
+                        </div>
+
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <img src="images/image1_7.webp" alt="">
+    </div>
+    <div class="flex flex-col gap-10">
+        <div class="flex items-center flex-col py-5 gap-5">
+            <span class="text-xl font-light font-serif">Tools of the trade
+            </span>
+
+            <h2 class="text-4xl tracking-wide">THE CARPENTER’S CORNER</h2>
+
+        </div>
+
+        <div class="w-full flex px-14 py-5 gap-10">
+            <div class="w-4/12 ">
+                <span class="text-[#D1A773] font-semibold text-md px-3">DIY KITS</span>
+                <p class="font-light tracking-wider">At TCSC, we empower carpenters and homeowners of all skill levels. Discover our DIY home products and take charge of your next renovation. Show the world what you're made of!</p>
+            </div>
+            <div class="w-4/12">
+                <span class="text-[#D1A773] font-semibold text-md px-3">HIGH QUALITY MATERIALS</span>
+                <p class="font-light tracking-wider">Explore endless design possibilities with our diverse selection of natural woods and vetted products. From exotic to locally-sourced wooden slabs, we have everything you need for your next design project. Shop top industry products today!</p>
+            </div>
+            <div class="w-4/12">
+                <span class="text-[#D1A773] font-semibold text-md px-3">EXPERT ADVISE</span>
+                <p class="font-light tracking-wider">Get expert advice directly from our shop. Explore our blog posts and videos! Browse our image gallery or contact our office for expert advice on your next project. We're here to help and empower you. With over a decade of experience, our team is dedicated to your success.</p>
+            </div>
+
+        </div>
+        <a href="" class="px-10 flex justify-center py-10">
+            <div class="border border-[#D1A773] text-sm px-10 py-3 text-[#d1a773] self-start font-normal">EXPLORE</div>
+        </a>
+    </div>
+    <div class="bg-[#0E3F54] w-full h-48">
+        <span>Our Custom Projects</span>
+        <a href="">
+
+        </a>
+    </div>
 </body>
+
 </html>
