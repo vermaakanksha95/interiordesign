ec671341f763b1e9366bcbea0fbf53e44d7083d3
ruchikushwah <ruchi741@bgpgmail.com>
2024-11-14 09:30:12 +0530

    commit

---
+++ carpenter.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+</head>
+
+<body>
+    <?php include "includes/header.php"; ?>
+    <div class="text-center">
+        <h2>The Carpenter's Corner</h2>
+        <span>IS FOR MAKERS JUST LIKE YOU</span>
+        <p>From the novice carpenter to the seasoned builder, you can find everything you need for your next interior design job or DIY project.</p>
+    </div>
+    <div class=" px-10 gap-5 grid grid-cols-3">
+        <div class="flex flex-col gap-2">
+            <img src="images/vieques-dining-table-vieques-organic-round-dining-table-made-from-maple-parota-monkey-pod-exotic-woodslab-38581276279010.webp" alt="">
+            <span class="self-end">Slabs</span>
+            <span class="flex items-center self-end">SHOP Slabs
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+
+            </span>
+        </div>
+        <div>
+            <img src="images/bathroom-vanity-24-inch-navy-blue-shaker-single-sink-bathroom-vanity-40505532285154.webp" alt="">
+            <span class="self-end">Slabs</span>
+            <span class="flex items-center self-end">SHOP Slabs
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+
+            </span>
+        </div>
+        <div>
+            <img src="images/slab-style-coffee-table-made-to-order-and-customizable-37942571237602_720x_15755538-0ebc-4b67-b19b-58a18c3bfbe9.webp" alt="">
+            <span class="self-end">Slabs</span>
+            <span class="flex items-center self-end">SHOP Slabs
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+
+            </span>
+        </div>
+    </div>
+</body>
+
+</html>
